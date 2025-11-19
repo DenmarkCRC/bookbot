117ff25935d7a761b55e518bca7cf139f25cedfa
DenmarkCRC <dennis@denmarkcrc.com.au>
2025-11-20 07:11:45 +0800

    passed CH3 L3 - added sys.argv functionality

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words, get_character_count, get_sorted_list
+import sys
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -6,7 +7,10 @@ def get_book_text(path_to_file):
     return file_contents
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     book_text = get_book_text(book_path)
     num_words = get_num_words(book_text)
     char_count = get_character_count(book_text)
