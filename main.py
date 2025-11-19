bfe32dbc33cc9db0adbe42ad6fd2abc891b3b691
DenmarkCRC <dennis@denmarkcrc.com.au>
2025-11-19 22:24:58 +0800

    completed ch3 lesson 1

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, get_character_count
+from stats import get_num_words, get_character_count, get_sorted_list
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -6,10 +6,18 @@ def get_book_text(path_to_file):
     return file_contents
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    book_path = "books/frankenstein.txt"
+    book_text = get_book_text(book_path)
     num_words = get_num_words(book_text)
     char_count = get_character_count(book_text)
+    sorted_chars = get_sorted_list(char_count)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    print(char_count)
+    print("--------- Character Count -------")
+    for char in sorted_chars:
+        if char["name"].isalpha():
+            print(f"{char["name"]}: {char["count"]}")
 
 main()
--- stats.py
+++ stats.py
@@ -8,3 +8,14 @@ def get_character_count(book):
     for char in book:
         char_dict[char.lower()] = char_dict.get(char.lower(), 0) + 1
     return char_dict
+
+def sort_on(items):
+    return items["count"]
+
+def get_sorted_list(dict):
+    list = []
+    for key in dict:
+        i = { "name": key, "count": dict[key]}
+        list.append(i)
+    list.sort(reverse=True, key=sort_on)
+    return list
